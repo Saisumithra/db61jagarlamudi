2192c4a6e689be012acb573408377bfdef140f8b
Saisumithra <77755086+Saisumithra@users.noreply.github.com>
2021-04-11 16:39:45 -0500

    assignment 12

--- controllers/flowerbouquet.js
+++ controllers/flowerbouquet.js
@@ -1,42 +1,56 @@
 var flowerbouquet = require('../models/flowerbouquet');
 // List of all flowerbouquet
-exports.flowerbouquet_list = async function(req, res) {
-    try{
+exports.flowerbouquet_list = async function (req, res) {
+    try {
         flowerbouquet = await flowerbouquet.find();
         res.send(flowerbouquet);
-        } catch(err){
-          res.status(500).send(`{"error": ${err}}`);
-        }
-};
-// for a specific flowerbouquet
-exports.flowerbouquet_detail = async function(req, res) {
-    console.log("detail"  + req.params.id)
-    try {
-        console.log("check")
-        result = await flowerbouquet.findById(req.params.id)
-        console.log(result)
-        res.send(result)
-    } catch (error) {
-       res.status(500).send(`{"error": document for id ${req.params.id} not found`);
+    } catch (err) {
+        res.send(`{"error": ${err}}`);
+        res.status(500);
     }
 };
-// Handle flowerbouquet create on POST.
-exports.flowerbouquet_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: flowerbouquet create POST');
+// for a specific flowerbouquet
+exports.flowerbouquet_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await flowerbouquet.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+};
+// Handle dog create on POST.
+exports.flowerbouquet_create_post = async function (req, res) {
+    console.log(req.body)
+    let document = new flowerbouquet();
+
+
+    document.flowername = req.body.flowername;
+    document.numberofflowers = req.body.numberofflowers;
+    document.deliverylocation = req.body.deliverylocation;
+
+    try {
+        let result = await document.save();
+        res.send(result);
+    } catch (err) {
+        res.send(`{"error": ${err}}`)
+        res.status(500);
+    }
 };
 // Handle flowerbouquet delete form on DELETE.
-exports.flowerbouquet_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: flowerbouquet delete DELETE ' + req.params.id);
+exports.flowerbouquet_delete = function (req, res) {
+    res.send('NOT IMPLEMENTED: flowerbouquet delete DELETE ' + req.params.id);
 };
 // Handle flowerbouquet update form on PUT.
-exports.flowerbouquet_update_put = async function(req, res) {
+exports.flowerbouquet_update_put = async function (req, res) {
     console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
     try {
         let toUpdate = await flowerbouquet.findById(req.params.id)
         // Do updates of properties
-        if(req.body.flowername) toUpdate.flowername = req.body.flowername;
-        if(req.body.numberofflowers) toUpdate.numberofflowers = req.body.numberofflowers;
-        if(req.body.deliverylocation) toUpdate.deliverylocation = req.body.deliverylocation;
+        if (req.body.flowername) toUpdate.flowername = req.body.flowername;
+        if (req.body.numberofflowers) toUpdate.numberofflowers = req.body.numberofflowers;
+        if (req.body.deliverylocation) toUpdate.deliverylocation = req.body.deliverylocation;
         let result = await toUpdate.save();
         console.log("Sucess " + result)
         res.send(result)
@@ -44,15 +58,90 @@ exports.flowerbouquet_update_put = async function(req, res) {
         res.status(500)
         res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
     }
- 
+
 };
 // Handle a show all view
-exports.flowerbouquet_view_all_Page = async function(req, res) {
-    try{
-    flowerbouquet = await flowerbouquet.find();
-    res.render('flowerbouquet', { title: 'flowerbouquet Search Results', results: flowerbouquet });
+exports.flowerbouquet_view_all_Page = async function (req, res) {
+    try {
+        flowerbouquet = await flowerbouquet.find();
+        console.log("njfndw")
+        res.render('flowerbouquet', {
+            title: 'flowerbouquet Search Results',
+            results: flowerbouquet
+        });
+    } catch (err) {
+        res.send(`{"error": ${err}}`);
+        res.status(500);
     }
-    catch(err){
-    res.error(500,`{"error": ${err}}`);
+};
+
+// Handle flowerbouquet delete on DELETE.
+exports.flowerbouquet_delete = async function (req, res) {
+    console.log("delete " + req.params.id)
+    try {
+        result = await flowerbouquet.findByIdAndDelete(req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
+};
+// Handle a show one view with id specified by query
+exports.flowerbouquet_view_one_Page = async function (req, res) {
+    console.log("single view for id " + req.query.id)
+    try {
+        result = await flowerbouquet.findById(req.query.id)
+        res.render('flowerdetail', {
+            title: 'flowerbouquet Detail',
+            toShow: result
+        });
+    } catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+// Handle building the view for creating a flowerbouquet.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.flowerbouquet_create_Page = function (req, res) {
+    console.log("create view")
+    try {
+        res.render('flowercreate', {
+            title: 'flowerbouquet Create'
+        });
+    } catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+// Handle building the view for updating a flowerbouquet.
+// query provides the id
+exports.flowerbouquet_update_Page = async function (req, res) {
+    console.log("update view for item " + req.query.id)
+    try {
+        let result = await flowerbouquet.findById(req.query.id)
+        res.render('flowerupdate', {
+            title: 'flowerbouquet Update',
+            toShow: result
+        });
+    } catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle a delete one view with id from query
+exports.flowerbouquet_delete_Page = async function (req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try {
+        result = await flowerbouquet.findById(req.query.id)
+        res.render('flowerdelete', {
+            title: 'flowerbouquet Delete',
+            toShow: result
+        });
+    } catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
     }
 };
--- routes/flowerbouquet.js
+++ routes/flowerbouquet.js
@@ -1,17 +1,24 @@
-// var express = require('express');
-// var router = express.Router();
+ var express = require('express');
+ const flower_controlers = require('../controllers/flowerbouquet');
+ var router = express.Router();
 
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('flowerbouquet', { title: 'Search Results' });
-// });
+ /* GET home page. */
+ router.get('/',flower_controlers.flowerbouquet_view_all_Page);
+/* GET detail flower page */
+router.get('/detail', flower_controlers.flowerbouquet_view_one_Page);
+/* GET create flower page */
+router.get('/create', flower_controlers.flowerbouquet_create_Page);
+/* GET create update page */
+router.get('/update', flower_controlers.flowerbouquet_update_Page);
+/* GET create flower page */
+router.get('/delete', flower_controlers.flowerbouquet_delete_Page);
 
-// module.exports = router;
+ module.exports = router;
 
 
-var express = require('express');
-const flowerbouquet_controllers= require('../controllers/flowerbouquet');
-var router = express.Router();
-/* GET costumes */
-router.get('/', flowerbouquet_controllers.flowerbouquet_view_all_Page );
-module.exports = router;
+//var express = require('express');
+//const flowerbouquet_controllers= require('../controllers/flowerbouquet');
+//var router = express.Router();
+/* GET flowers */
+//router.get('/', flowerbouquet_controllers.flowerbouquet_view_all_Page );
+//module.exports = router;
